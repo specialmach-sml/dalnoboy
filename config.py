b0935192290506bdf5c0dfa47bc99de31e721739
specialmach-sml <specialmach@mail.ru>
2026-05-17 06:40:00 +0000

    load config from env

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
 
-TOKEN = os.getenv("BOT_TOKEN", "8634997756:AAEQziuv7zogJZYk3ZLk85JPKvvHYmU9UMQ")
+load_dotenv()
+
+TOKEN = os.getenv("BOT_TOKEN", "TEMP_TOKEN")
 DB_DSN = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@127.0.0.1:5432/dalnoboy")
