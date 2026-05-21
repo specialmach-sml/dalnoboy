cdf8d643491af782f8df64223a148c1a2fe7bc4f
specialmach-sml <specialmach@mail.ru>
2026-05-21 05:58:07 +0000

    Hide stale trucks on map

--- server/index.js
+++ server/index.js
@@ -187,6 +187,7 @@ app.get("/api/trucks/active", async (req, res) => {
       WHERE t.latitude IS NOT NULL
         AND t.longitude IS NOT NULL
         AND t.status='active'
+        AND t.location_updated_at > now() - interval '24 hours'
       ORDER BY t.location_updated_at DESC NULLS LAST
       LIMIT 500
     `);
